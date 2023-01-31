74547665eaeca3c7f668462364e3e12af7420ee8
Mendesjar <szrfft2dhf@privaterelay.appleid.com>
2023-01-31 09:21:47 -0300

    build: create project

---
+++ code-wise-clone/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.tsx"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: "Lufga,Verdana,sans-serif",
+      },
+    },
+  },
+  plugins: [],
+};
